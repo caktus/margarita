27167c765030770fe50e19a388af628b7cbc7c32
Scott Morningstar <scottm@caktus011.caktus.lan>
2013-07-26 14:52:04 -0400

    Pointed solr-install.sh to files.caktusgroup.com after Apache removed 3.6.2 from downloads

--- solr/solr-install.sh
+++ solr/solr-install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [ ! -d ./apache-solr-3.6.2 ]
 then
-    wget http://apache.mirrors.tds.net/lucene/solr/3.6.2/apache-solr-3.6.2.tgz
+    wget http://files.caktusgroup.com/media/apache-solr-3.6.2.tgz
     tar xvzf apache-solr-3.6.2.tgz
     rm apache-solr-3.6.2.tgz
 fi
