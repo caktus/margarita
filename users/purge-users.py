5e17dfe99944354087fcb35c447b98e1c80ff6b2
Calvin Spealman <calvin@caktusgroup.com>
2016-02-24 15:41:30 -0500

    [DJAN-22] More information when bailing on which users would have been removed

--- users/purge-users.py
+++ users/purge-users.py
@@ -32,7 +32,7 @@ def main():
     existing_devs = list_devs()
     users_to_drop = [user for user in existing_devs if user not in options.keep_users]
     if not set(existing_devs) - set(users_to_drop):
-        print("Error: refusing to remove all accounts")
+        print("Error: refusing to remove all accounts: %s" % ", ".join(users_to_drop))
         sys.exit(1)
     else:
         for username in users_to_drop:
