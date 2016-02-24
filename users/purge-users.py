c139153b695e21d924bc593ce464c61f475bbeac
Calvin Spealman <calvin@caktusgroup.com>
2016-02-24 10:36:31 -0500

    [DJAN-22] improving the purge script

--- users/purge-users.py
+++ users/purge-users.py
@@ -4,34 +4,35 @@ from __future__ import print_function
 import sys
 import re
 import os
+import pwd
 from subprocess import check_output
 import optparse
 
 import yaml
 
-def list_users():
-    users = check_output("cut -d: -f1 /etc/passwd", shell=True).split('\n')
-    return users
-
 def list_devs():
-    users = list_users()
+    users = pwd.getpwall()
     devs = [user for user in users if get_authorized_keys_filepath(user)]
     return devs
 
-def get_authorized_keys_filepath(username):
-    return os.path.exists(os.path.join('/', 'home', username, '.ssh', 'authorized_keys'))
+def get_authorized_keys_filepath(user):
+    home_dir = user.pw_dir
+    path = os.path.join(home_dir, '.ssh', 'authorized_keys'))
+    if os.path.exists(path):
+        return path
 
 def main():
     parser = optparse.OptionParser()
     parser.add_option('', '--keep', dest="keep_users", action="append")
     (options, args) = parser.parse_args()
 
+    if not options.keep_users:
+        parser.error("At least one --keep option required. Refusing to purge ALL developers.")
+
     users_to_drop = []
-    for username in list_users():
-        authorized_keys = get_authorized_keys_filepath(username)
-        if authorized_keys:
-            if username not in keep_users:
-                users_to_drop.append(username)
+    for username in list_devs():
+        if username not in options.keep_users:
+            users_to_drop.append(username)
     existing_devs = list_devs()
     if not set(existing_devs) - set(users_to_drop):
         print("Error: refusing to remove all accounts")
